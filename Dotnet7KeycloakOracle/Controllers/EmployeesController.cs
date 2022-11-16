b175da27097fd33d824677a170a9166ad62bd8d5
Alex Vitorino <halexsrv@gmail.com>
2022-11-16 12:46:16 -0300

    Initial commit

---
+++ Dotnet7KeycloakOracle/Controllers/EmployeesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Serialization;
+using Dotnet7KeycloakOracle.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Dotnet7KeycloakOracle.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeesController
+{
+    private const string ConString = "User Id=system;Password=oracle; Data Source=localhost:1521/xe";
+
+    [HttpGet]
+    public string Get()
+    {
+        using var con = new OracleConnection(ConString);
+        
+        using var cmd = con.CreateCommand();
+        
+        try
+        {
+            con.Open();
+            cmd.BindByName = true;
+
+            cmd.CommandText = "SELECT * FROM HR.EMPLOYEES";
+
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            // while (reader.Read())
+            // {
+            //     Console.WriteLine($"Employee First Name: {reader["FIRST_NAME"]}");
+            // }
+
+            var converter = new Converter();
+
+            var rows = converter.ConvertToDictionary(reader);
+
+            return JsonConvert.SerializeObject(rows, Formatting.Indented);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
---
+++ Dotnet7KeycloakOracle/Utils/Converter.cs
@@ -0,0 +1,24 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace Dotnet7KeycloakOracle.Utils;
+
+public class Converter
+{
+    public IEnumerable<Dictionary<string, object>> ConvertToDictionary(OracleDataReader reader)
+    {
+        var columns = new List<string>();
+        var rows = new List<Dictionary<string, object>>();
+
+        for (var i = 0; i < reader.FieldCount; i++)
+        {
+            columns.Add(reader.GetName(i));
+        }
+
+        while (reader.Read())
+        {
+            rows.Add(columns.ToDictionary(column => column, column => reader[column]));
+        }
+
+        return rows;
+    }
+}
